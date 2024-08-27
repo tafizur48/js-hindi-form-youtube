4a18e2ef9c87a3d54dde7ea946d3a697258a029b
Tafizur Rahman <168195446+tafizur48@users.noreply.github.com>
2024-08-27 09:59:08 +0000

    variable config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1254214
+let accountEmail = "Mustafizur@gmail.com"
+var accountPassword = "1254"
+accountCity = "Delhi"
+let accountState
+
+//accountId = 5
+
+accountEmail = "mustafks.com"
+accountPassword = "21212121"
+accountCity = "assam"
+//javascript mey kus na likenese a undefined ho jata hain
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
