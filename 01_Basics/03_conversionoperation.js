327e6af84badaf381a05352ec0e8d16f7154638d
Tafizur Rahman <168195446+tafizur48@users.noreply.github.com>
2024-08-27 17:30:37 +0000

    conversion

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -16,7 +16,7 @@
 
 let isLoggedIn = "Tafizur"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true 0 => false
 // "" => false
@@ -25,8 +25,49 @@ console.log(booleanIsLoggedIn);
 let someNumber = 52
 
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
 
-// StringNumber => true 
+// StringNumber => true    
 
+// ***********operation*****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// negative value assign then given -ve value
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);//power
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "hello"
+let str2 = " tafizur"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// conversion
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "4"); //to add with string
+
+// console.log((3 + 4) * 5 % 3);
+
+console.log(+true); 1
+console.log(+"");//null means 0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 //not useable
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
