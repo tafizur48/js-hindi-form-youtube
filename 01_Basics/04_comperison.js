9de870451e59405e542c62056a0504d597cfe250
Tafizur Rahman <168195446+tafizur48@users.noreply.github.com>
2024-08-27 17:47:34 +0000

    coparision

---
+++ 01_Basics/04_comperison.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// avoid this type of conversion
+// console.log(null > 0);//kabhi null mey hota hai ya NaN mey hota hai
+// console.log(null == 0);
+// console.log(null >= 0);//null is converted to number
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+
+// strick check ===
+
+console.log("2" === 2);
+
+
