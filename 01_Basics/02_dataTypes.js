073706f591ff9427c35df76128b6a2039aa2146e
Tafizur Rahman <168195446+tafizur48@users.noreply.github.com>
2024-08-27 17:00:19 +0000

    data conversionn operation

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all js code as newer version
+//alert(3+9) //we are using node js,not browser
+
+// datatypes
+let name = "Tafizur"
+let age = 18
+let isloggedin = false
+
+// Primitive Data types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value its means not given value
+// undefine => its means value aya nhi
+// symbol => unique
+
+// object
+
+console.log(typeof "tafizur")
+console.log(typeof undefined);//undefine
+console.log(typeof null);//object
+
---
+++ 01_Basics/03_conversionoperation.js
@@ -0,0 +1,32 @@
+ let score = "44abc "
+
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+ let valueInNumber = Number(score)//number capital use
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "44" => 44
+// "44abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = "Tafizur"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true 0 => false
+// "" => false
+// "tafizur" => true
+
+let someNumber = 52
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+// StringNumber => true 
+
