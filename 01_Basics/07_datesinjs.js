a61568446a359b5c8198ecd2c00cc471914579f7
Tafizur Rahman <168195446+tafizur48@users.noreply.github.com>
2024-08-28 10:05:46 +0000

    dates

---
+++ 01_Basics/07_datesinjs.js
@@ -0,0 +1,31 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);//object
+
+
+//let myCreateDate = new Date(2024, 0, 28)//month is starting 0 se
+//let myCreateDate = new Date(2024, 0, 28,5,3)
+//let myCreateDate = new Date("2024-08-28")
+//console.log(myCreateDate.toDateString());
+//console.log(myCreateDate.toLocaleString());
+
+let myCreateDate = new Date("08-28-2024")
+//console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());//millisecond
+
+// console.log(Math.floor(Date.now()));
+
+
+
+
+
+
+
