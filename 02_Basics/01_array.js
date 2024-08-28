cee45ff0023e7a604ac41cd76065ccad1b92857b
Tafizur Rahman <168195446+tafizur48@users.noreply.github.com>
2024-08-28 10:06:04 +0000

    array part1

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,50 @@
+//array decleration
+
+const myArr = [0, 2, 6, 1, 9]
+const myHeoro = ["shaktiman, marvel"]
+
+const myArr2 = new Array(1, 2, 3, 9,)
+// console.log(myArr[1]);
+
+
+//console.log(myArr[0]);
+
+// Merhod of Array
+// myArr.push(6)//add the array
+// myArr.push(7)
+// myArr.pop(6)//delete the array
+
+//myArr.unshift(5) //insert in first when using unshift
+//myArr.shift()//first value delete 
+
+
+// *******************operation**************
+
+//console.log(myArr.includes(4)); //jis value exist nhi korta
+// means given nhi hain array mey woh false 
+
+//console.log(myArr.indexOf(3));
+
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+
+// slice , splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("c ", myArr);
+console.log(myn2);
+
+
+
+
