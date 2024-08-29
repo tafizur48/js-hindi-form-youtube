f0d53b374aebd7d836ec9f762551dd7448e9abe5
Tafizur Rahman <168195446+tafizur48@users.noreply.github.com>
2024-08-29 11:00:21 +0000

    ifelse condition

---
+++ 04_control-flow/01.js
@@ -0,0 +1,72 @@
+// if condition
+
+// if (condition) {
+    
+// }
+//const isUserloggedIn = true
+const tempressure = 41
+
+if (tempressure < 40) {
+   // console.log("less than 50");
+    
+}else{
+//console.log("tempressure is greater then 50");
+
+}
+
+// console.log("tempressure is greater then 50");
+
+// if (2 == 2) {
+    
+// }
+
+// comparision <, >, <=, >=, ==, !=, === it mens check
+
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power : ${power}`);
+    
+    
+// }
+ // block scop
+// console.log(`user power : ${power}`);//its all about scop its out of scop
+
+const balance = 1000
+
+//if (balance > 500) console.log("test");//ak line mey is called implicit
+
+// nesting loop
+
+// if (balance < 500) {
+//     console.log("less than");
+    
+// }else if (balance < 750) {
+//     console.log("less than 750");
+    
+    
+// }else if (balance < 900) {
+//     console.log("lesst then 900");
+    
+// }else{
+//     console.log("less then 1200");
+    
+// }
+// and condition
+const isUserloggedIn = true
+const debitCard = true
+const loggedInGoogle = false
+const loggedInEmail = true
+
+if (isUserloggedIn && debitCard) {
+    console.log("Allow to buy course");
+    
+}
+// or condition
+if (loggedInGoogle || loggedInEmail) {
+    console.log("user logged in");
+    
+    
+}
