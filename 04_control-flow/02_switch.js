563f835fc0f35db41e6c245e96ecc3f64635d709
Tafizur Rahman <168195446+tafizur48@users.noreply.github.com>
2024-08-29 11:00:43 +0000

    switch case

---
+++ 04_control-flow/02_switch.js
@@ -0,0 +1,59 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//const month = 3
+//switch (month) {
+//     case 1:
+//         console.log("january");
+        
+//         break;
+//     case 2:
+//         console.log("fab");
+        
+//         break;
+//     case 3:
+//         console.log("march");
+        
+//         break;
+//     case 4:
+//         console.log("april");
+        
+//         break;
+
+//     default:
+//         console.log("does not ,match");
+        
+//         break;
+// }
+
+// agr string hai toh
+
+const month = "april"
+switch (month) {
+    case "jan":
+        console.log("jan");
+        
+        break;
+    case "fab":
+        console.log("fab");
+        
+        break;
+    case "march":
+        console.log("march");
+
+        break;
+    case "april":
+        console.log("april");
+
+        break;
+
+
+    default:
+        break;
+}
